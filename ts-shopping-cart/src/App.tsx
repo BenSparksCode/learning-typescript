968b76a892eec2838ef1e38b78db47c0d7ffd90d
Ben Sparks <bensparks0@gmail.com>
2021-03-20 11:50:53 +0200

    Adding API call to fakestore API

--- ts-shopping-cart/src/App.tsx
+++ ts-shopping-cart/src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {useQuery} from 'react-query';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 
 // Components
 import Drawer from '@material-ui/core/Drawer';
@@ -9,9 +9,38 @@ import Badge from '@material-ui/core/Badge';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 // Styles
+import { Wrapper } from './App.styles';
+
+// Types
+export type CardItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CardItemType[]> => {
+  const headers = new Headers();
+  headers.set('Access-Control-Allow-Origin', '*');
+  const init: RequestInit = {
+    mode: 'no-cors',
+    credentials: 'omit',
+    headers,
+  }
+  return await (await fetch('https://fakestoreapi.com/products',
+    init
+  )).json()
+}
 
 
 const App = () => {
+
+  const { data, isLoading, error } = useQuery<CardItemType[]>('products', getProducts)
+  console.log('here', data);
+
   return (
     <div className="App">
       start
