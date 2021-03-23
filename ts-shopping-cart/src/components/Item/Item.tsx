4b5fc3f14f7b09d778e59d5210b242a6eca2823d
Ben Sparks <bensparks0@gmail.com>
2021-03-23 14:22:14 +0200

    Built out Item component

--- ts-shopping-cart/src/components/Item/Item.tsx
+++ ts-shopping-cart/src/components/Item/Item.tsx
@@ -1,4 +1,4 @@
-import {} from 'react'
+
 import Button from '@material-ui/core/Button'
 
 // Types 
@@ -6,11 +6,31 @@ import { CartItemType } from '../../App'
 // Styles
 import { Wrapper } from './Item.styles'
 
+type Props = {
+    item: CartItemType,
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
 
-export const Item = () => {
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+
+    console.log(item)
+
     return (
-        <div>
-            Item
-        </div>
+        <Wrapper>
+            <img
+                src={item.image.replace(
+                    'https://fakestoreapi.com',
+                    'https://fakestoreapi.herokuapp.com/')}
+                alt={item.title} />
+            <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <h3>${item.price}</h3>
+            </div>
+            <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+        </Wrapper>
     )
 }
+
+export default Item
