555ef479b7322d054219162fe6b114c499fba940
Ben Sparks <bensparks0@gmail.com>
2021-03-27 16:23:17 +0200

    Built out Cart component

--- ts-shopping-cart/src/components/Cart/Cart.tsx
+++ ts-shopping-cart/src/components/Cart/Cart.tsx
@@ -3,14 +3,29 @@
 import CartItem from '../CartItem/CartItem'
 
 // Types 
+import { CartItemType } from '../../App'
 
 // Styles
 import { Wrapper } from './Cart.styles'
 
-export const Cart = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+type Props = {
+    cartItems: CartItemType[],
+    addToCart: (clickedItem: CartItemType) => void,
+    removeFromCart: (id: number) => void,
 }
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+
+
+
+
+    return <Wrapper>
+        <h2>Your shopping cart</h2>
+        {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+        {cartItems.map(item =>(
+            <CartItem />
+        ))}
+    </Wrapper>
+}
+
+export default Cart
