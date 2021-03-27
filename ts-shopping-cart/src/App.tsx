7a20cc96f3312c782ce9b1ce2e3ff91c426ec0f3
Ben Sparks <bensparks0@gmail.com>
2021-03-27 16:50:20 +0200

    Implemented addToCart functionality on shop items

--- ts-shopping-cart/src/App.tsx
+++ ts-shopping-cart/src/App.tsx
@@ -38,7 +38,21 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((acc: number, item) => acc + item.amount, 0)
 
-  const handleAddToCart = (clickedItem: CartItemType) => null
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      // Is item already in cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+      if (isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        ))
+      }
+      // First time item added to cart
+      return [...prev, { ...clickedItem, amount: 1 }]
+    })
+  }
 
   const handleRemoveFromCart = () => null
 
